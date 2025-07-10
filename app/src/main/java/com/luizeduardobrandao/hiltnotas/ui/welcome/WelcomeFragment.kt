e8608e2b669fbee482792a7d9e77f626716b160b
EduardoBran <eduardo.ads1814@gmail.com>
2025-07-10 09:12:37 -0300

    atualizando WelcomeFragment para exibição da primeira pagina

--- app/src/main/java/com/luizeduardobrandao/hiltnotas/ui/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/luizeduardobrandao/hiltnotas/ui/welcome/WelcomeFragment.kt
@@ -1,31 +1,46 @@
 package com.luizeduardobrandao.hiltnotas.ui.welcome
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.luizeduardobrandao.hiltnotas.R
+import com.luizeduardobrandao.hiltnotas.databinding.FragmentWelcomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+// * Fragment de boas-vindas, sem menu.
+
+@AndroidEntryPoint
 class WelcomeFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = WelcomeFragment()
-    }
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
 
-    private val viewModel: WelcomeViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    // private val viewModel: WelcomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+
+        // Infla o layout de boas-vindas
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Navega para a lista ao clicar em "Iniciar"
+        binding.buttonStart.setOnClickListener {
+            findNavController().navigate(R.id.action_welcome_to_list)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
